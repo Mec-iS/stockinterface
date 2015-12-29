1b51292c7f45b5104f847553b0b5ec2660495e05
Lorenzo <tunedconsulting@gmail.com>
2015-12-29 20:44:45 +0100

    Add __str__ and __repr__ for OredersBook. _build_order_* now returns a Request, to match with the new methods in play.py

--- game.py
+++ game.py
@@ -71,7 +71,23 @@ class OrdersBook:
         self.orders_failed = []     # orders failed
 
         # load the stocks list
+        print('Fetching stocks list')
         #self.list_stocks()
+
+    def __str__(self):
+        return str("{__class__.__name__}: {agent!r} is using "
+                   "this orders book at {venue!r}").format(
+            __class__=self.__class__,
+            agent=self.agent,
+            venue=self.venue
+        )
+
+    def __repr__(self):
+        return "{__class__.__name__}: {id!r}, hash={hash!r}".format(
+            __class__=self.__class__,
+            id=id(self),
+            hash=hash(self)
+        )
 
     def list_stocks(self):
         """List the stocks available for trading on a venue and store them in
@@ -87,10 +103,17 @@ class OrdersBook:
             url=endpoint,
             attr='listed')
 
-    def _build_order_data(self, order):
-        """Build request's data to perform a ask/bid action, data is taken from
-        the Order's constructor."""
+    def _build_order_request(self, order):
+        """Build request's data to perform a buy/sell, data is taken from
+        the Order's constructor and a Request object is returned."""
         import json
+        from client import _request
+
+        # find the right endpoint URL
+        endpoint = Stocks(
+            stock=order.order_payload['stock'],
+            venue=order.order_payload['venue']
+        ).submit_order
 
         post_data = {
             "account": self.account,
@@ -98,26 +121,23 @@ class OrdersBook:
         }
         post_data.update(order.order_payload)
 
-        return json.dumps(post_data)
+        return _request(endpoint, json.dumps(post_data))
 
     def place_order(self, order):
-        """Build and send an order via the async loop. Create an Endpoint instance,
+        """Build and send an order via the async loop. Create an Endpoint instance
         """
         print(repr(self) + " is passed to Dispatcher")
-        # find the right endpoint URL
-        endpoint = Stocks(
-            stock=order.order_payload['stock'],
-            venue=order.order_payload['venue']
-        ).submit_order
         # send to dispatcher
-        Play().dispatch(
+        result = Play().dispatch(
             caller=self,
-            url=endpoint,
-            payload=self._build_order_data(order)
+            url=self._build_order_request(order)
         )
-
         # add order to sent orders in the orders books
-        setattr(self, 'orders_sent', order)
+        setattr(self, 'orders_sent', getattr(self, 'order_sent').append(order))
+
+        print(result)
+        # #todo: a function for the feedback of orders' submissions:
+        # #todo: result can be 'shipped', 'queued' or 'rejected'
 
 
 class Order:
@@ -127,9 +147,11 @@ class Order:
 
     def __init__(self, orders_book):
         """
-        Construct a order to be sent.
+        Construct a order to be sent. It needs only an OrdersBook instance
+        to be passed. The parameters of the order can be passed in a separate
+        call to `Order.cook_order()`
 
-        Example of order to be sent as payload to the endpoint:
+        Example of parameters for a order:
             {
                 "account": Agent.account
                 "venue": Agent.venue,
@@ -151,6 +173,7 @@ class Order:
         self.account = orders_book.account
 
         # to be set when the order is 'cooked'
+        # #todo: using @property ?
         self.order_payload = None
 
     def __str__(self):
@@ -182,6 +205,8 @@ class Order:
             attr='orders')
 
     def cook_order(self, **kwargs):
+        """Check that all the parameters are submitted and store the dictionary in
+        a class attribute"""
         assert all(kwargs.get(k) for k in self.params)
         setattr(self, 'order_payload', kwargs)
 
