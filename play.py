5bed5068c1729861e588c7ef95d8667a0fd398f2
Lorenzo <tunedconsulting@gmail.com>
2015-12-29 01:53:16 +0100

    Refactor coroutine and added caller/attr arguments

--- play.py
+++ play.py
@@ -4,13 +4,14 @@ Levels playing module
 """
 import asyncio
 import json
-from endpoints import StockFighter
+from time import time
+
 from async_client import fetch
 
 __author__ = 'Lorenzo'
 
 
-class Play(StockFighter):
+class Play:
     """
     General class for levels. Each subclass represents a level.
 
@@ -18,7 +19,8 @@ class Play(StockFighter):
     def __init__(self, agent):
         # Unique account assigned by level's instruction
         self.agent = agent
-        # to be set when loop starts
+        # to be set pointing to the running loop when started
+        # see self.start_play_loop()
         self.play_loop = None
 
         # players get a user account number at the beginning of each level
@@ -27,17 +29,13 @@ class Play(StockFighter):
         # Tell Tornado to use the asyncio eventloop
         AsyncIOMainLoop().install()
 
-        self.http_client = None
+        self.start_play_loop()
 
     def start_play_loop(self):
-        from tornado.httpclient import AsyncHTTPClient
 
         # get the loop policy
         loop = asyncio.get_event_loop()
         loop.set_debug(True)
-
-        # create Tornado HTTP Async client
-        self.http_client = AsyncHTTPClient()
 
         # start the loop and inject coroutine
         loop.run_forever()
@@ -49,37 +47,54 @@ class Play(StockFighter):
         setattr(self, 'play_loop', None)
 
     @asyncio.coroutine
-    def hit_endpoint(self, caller, mode):
+    def hit_endpoint(self, url, data=None, caller=None, attr=None):
         """
-        Coroutine to fetch the endpoint
-        :param apimodels.Endpoint:
-        :param tornado.httpclient.AsyncHTTPClient http_client:
-        :return None: set `response` attribute in the calling class
+        Coroutine invoked to fetch the endpoint.
+
+        :param object caller: the object requiring the data and in which the
+                        response's body will be stored
+        :param str url: url to be hit
+        :param str attr: the attribute's name from the caller in which the
+                        response's body will be stored
+        :param dict data: POST data if needed
+        :return dict: response's body
         """
         response = yield from fetch(
-            self.http_client,
-            caller.url
+            url,
+            data=data
         )
         read = response.body.decode()
         read = json.loads(read)
         print(read)
         if not read["ok"]:
             raise ValueError('play.hit_endpoint(). "ok" value is false,'
-                             'there is an error in the payload: ' + str(read["error"]))
-        # storing the response body in the class object
-        return setattr(
-            caller,
-            mode,
-            read)
+                             'there is an error in the response: ' + str(read["error"]))
 
-    def get(self, caller, mode):
-        """
+        # set the attribute to store the result in the caller,
+        # using different methods depending on attribute's type.
+        # #todo: to be moved in a 'response receiving' method in the caller or
+        # #todo: should emit an event to caller's listener
+        if caller and attr:
+            to_set = getattr(caller, attr)
+            {
+                'list': to_set.append(time(), read),
+                'dict': setattr(caller, attr, read),
+                'NoneType': setattr(caller, attr, read)
+            }.get(
+                type(getattr(caller, attr)).__name__
+            )
 
-        :param Endpoint caller:
-        :param str mode:
-        :return:
+    def dispatch(self, caller, url, attr, payload=None):
         """
-        response = yield from self.play_loop.create_task(
-            self.hit_endpoint(caller, mode)
+        Collect the request and add the coroutine to the loop.
+
+        :param object caller: the object requiring the data and in which the
+                        response's body will be stored
+        :param str url: url to be hit
+        :param str attr: the attribute's name from the caller in which the
+                        response's body will be stored
+        :return: a Future
+        """
+        yield from self.play_loop.create_task(
+            self.hit_endpoint(url, payload, caller, attr)
         )
-        return response
