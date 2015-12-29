3000149871236864db2f2552fbdb1392599da0f9
Lorenzo <tunedconsulting@gmail.com>
2015-12-29 20:46:57 +0100

    Separate objects tests from (sync) fetching tests

--- testing/tests_HTTP.py
+++ testing/tests_HTTP.py
@@ -3,37 +3,69 @@
 Unit Tests module for Endpoints' URLs fetching
 """
 import unittest
-import asyncio
+from unittest import mock
+
 __author__ = 'Lorenzo'
 
 
-class Test_API(unittest.TestCase):
+# (account, venue, stock)
+tests = [
+    ('YCH86804436', 'SSDX', 'PMI'),
+    ('WCF86833536', 'CDKX', 'FRI'),
+    ('RTS86811456', 'DTEX', 'RTY')
+]
+
+from endpoints import APIHeartBeat
+from client import _request, _response
+class Test_Enpoint_APIHeartBeat(unittest.TestCase):
     def setUp(self):
-        from endpoints import APIHeartBeat
         self.endpoint = APIHeartBeat()
 
-    def test_the_api_should_heartbeat(self):
-        self.endpoint._get()
-        if not self.endpoint.response["ok"]:
-            assert False
-        assert True
+    def test_should_create_endpoint(self):
+        print(self.endpoint)
+        assert isinstance(self.endpoint, APIHeartBeat)
+        assert self.endpoint.url == 'https://api.stockfighter.io/ob/api/heartbeat'
+
+    def test_should_fetch_endpoint(self):
+        print('>>> heartbeating')
+        body, _ = _response(_request(self.endpoint.url))
+        print('>>> response: ' + str(body))
+        assert body["ok"]
 
     def tearDown(self):
         del self.endpoint
 
 
-class Test_Venue(unittest.TestCase):
-    test = ['CDKEX']
+from endpoints import VenueHeartBeat
+class Test_Enpoint_VenueHeartBeat(unittest.TestCase):
+    tests = tests
 
     def setUp(self):
-        from endpoints import VenueHeartBeat
-        self.endpoint = VenueHeartBeat(self.test[0])
+        self.endpoint = VenueHeartBeat(tests[0][1])
 
-    def test_the_api_should_heartbeat(self):
-        self.endpoint._get()
-        if not self.endpoint.response["ok"]:
-            assert False
-        assert True
+    def test_should_create_endpoint(self):
+        print(self.endpoint)
+        assert isinstance(self.endpoint, VenueHeartBeat)
+        assert self.endpoint.url == 'https://api.stockfighter.io/ob/api/venues/SSDX/heartbeat'
+
+    def tearDown(self):
+        del self.endpoint
+
+
+from endpoints import Stocks
+class Test_Enpoint_Stocks(unittest.TestCase):
+    tests = tests
+
+    def setUp(self):
+        self.endpoint = Stocks(
+            venue=tests[0][1],
+            stock=tests[0][2]
+        )
+
+    def test_should_create_endpoint(self):
+        assert self.endpoint.venue == tests[0][1] and self.endpoint.stock == tests[0][2]
+        assert all(getattr(self.endpoint, v[0], None) for v in self.endpoint.actions)
+        print(self.endpoint)
 
     def tearDown(self):
         del self.endpoint
@@ -41,12 +73,11 @@ class Test_Venue(unittest.TestCase):
 
 def suite():
     s = unittest.TestSuite()
-    s.addTests(unittest.defaultTestLoader.loadTestsFromTestCase(Test_API))
-    s.addTests(unittest.defaultTestLoader.loadTestsFromTestCase(Test_Venue))
+    s.addTests(unittest.defaultTestLoader.loadTestsFromTestCase(Test_Enpoint_APIHeartBeat))
+    s.addTests(unittest.defaultTestLoader.loadTestsFromTestCase(Test_Enpoint_VenueHeartBeat))
+    s.addTests(unittest.defaultTestLoader.loadTestsFromTestCase(Test_Enpoint_Stocks))
     return s
 
-# The typical main program for a test module.
-# ::
 
 if __name__ == "__main__":
     t = unittest.TextTestRunner()
--- testing/tests_objects.py
+++ testing/tests_objects.py
@@ -8,13 +8,13 @@ from unittest import mock
 __author__ = 'Lorenzo'
 
 
-global play_loop
 # (account, venue, stock)
 tests = [
     ('YCH86804436', 'SSDX', 'PMI'),
     ('WCF86833536', 'CDKX', 'FRI'),
     ('RTS86811456', 'DTEX', 'RTY')
 ]
+
 
 from game import Agent, OrdersBook, Order
 class Test_Agent_Order(unittest.TestCase):
@@ -27,83 +27,37 @@ class Test_Agent_Order(unittest.TestCase):
         "orderType": "limit"
     }
 
-    def setUp(self):
-        self.agent = agent =  Agent(self.tests[0][0], self.tests[0][1])
-        self.orders_book = orders_book = OrdersBook(agent)
-        self.order = Order(orders_book)
+    @classmethod
+    def setUpClass(cls):
+        cls.agent = Agent(cls.tests[0][0], cls.tests[0][1])
+        cls.orders_book = OrdersBook(cls.agent)
+        cls.order = Order(cls.orders_book)
 
     def test_should_create_agent(self):
-        print(self.agent)
-        assert self.agent.account == self.tests[0][0]
-        assert self.agent.venue == self.tests[0][1]
+        agent = self.agent
+        print(agent)
+        assert agent.account == self.tests[0][0]
+        assert agent.venue == self.tests[0][1]
+
+    def test_should_create_orders_book(self):
+        orders_book = self.orders_book
+        print(orders_book)
 
     def test_should_create_order(self):
-        print(self.order)
-        assert self.order.agent == self.agent
-        assert self.order.account == self.agent.account
-        assert self.order.venue == self.agent.venue
+        order = self.order
+        print(order)
+        assert order.agent == self.agent
+        assert order.account == self.agent.account
+        assert order.venue == self.agent.venue
 
-    def tearDown(self):
-        del self.agent
-        del self.order
-
-
-from endpoints import APIHeartBeat
-class Test_Enpoint_APIHeartBeat(unittest.TestCase):
-    def setUp(self):
-
-        self.endpoint = APIHeartBeat()
-
-    def test_should_create_endpoint(self):
-        print(self.endpoint)
-        assert isinstance(self.endpoint, APIHeartBeat)
-        assert self.endpoint.url == 'https://api.stockfighter.io/ob/api/heartbeat'
-
-    def tearDown(self):
-        del self.endpoint
-
-
-from endpoints import VenueHeartBeat
-class Test_Enpoint_VenueHeartBeat(unittest.TestCase):
-    tests = tests
-
-    def setUp(self):
-        self.endpoint = VenueHeartBeat(tests[0][1])
-
-    def test_should_create_endpoint(self):
-        print(self.endpoint)
-        assert isinstance(self.endpoint, VenueHeartBeat)
-        assert self.endpoint.url == 'https://api.stockfighter.io/ob/api/venues/SSDX/heartbeat'
-
-    def tearDown(self):
-        del self.endpoint
-
-
-from endpoints import Stocks
-class Test_Enpoint_Stocks(unittest.TestCase):
-    tests = tests
-
-    def setUp(self):
-        self.endpoint = Stocks(
-            venue=tests[0][1],
-            stock=tests[0][2]
-        )
-
-    def test_should_create_endpoint(self):
-        assert self.endpoint.venue == tests[0][1] and self.endpoint.stock == tests[0][2]
-        assert all(getattr(self.endpoint, v[0], None) for v in self.endpoint.actions)
-        print(self.endpoint)
-
-    def tearDown(self):
-        del self.endpoint
+    @classmethod
+    def tearDownClass(cls):
+        del cls
 
 
 def suite():
     s = unittest.TestSuite()
     s.addTests(unittest.defaultTestLoader.loadTestsFromTestCase(Test_Agent_Order))
-    s.addTests(unittest.defaultTestLoader.loadTestsFromTestCase(Test_Enpoint_APIHeartBeat))
-    s.addTests(unittest.defaultTestLoader.loadTestsFromTestCase(Test_Enpoint_VenueHeartBeat))
-    s.addTests(unittest.defaultTestLoader.loadTestsFromTestCase(Test_Enpoint_Stocks))
     return s
 
 
